01d0211c3e4ee58e64a1d2018b9689bf8df85a6f
matheus jordan <matheusj1599@gmail.com>
2020-05-18 19:59:32 -0300

    Adicionado guarda nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PortifolioModule } from './pages/portifolio/portifolio.module';
+import { UserModule } from './pages/user/user.module';
+import { BeaconModule } from './pages/beacon/beacon.module';
+import { LoginModule } from './pages/login/login.module';
+import { StatisticModule } from './pages/statistic/statistic.module';
+import { AuthGuard } from './shared/services/auth.guard';
+
 
 const routes: Routes = [
-  { path: 'portifolio', loadChildren: () => PortifolioModule }
+  { path: 'statistic', loadChildren: () => StatisticModule, canActivate: [AuthGuard] },
+  { path: 'user', loadChildren: () => UserModule, canActivate: [AuthGuard] },
+  { path: 'beacon', loadChildren: () => BeaconModule, canActivate: [AuthGuard] },
+  { path: 'login', loadChildren: () => LoginModule },
+  { path: '', redirectTo: 'login', pathMatch: 'prefix' }
 ];
 
 @NgModule({
